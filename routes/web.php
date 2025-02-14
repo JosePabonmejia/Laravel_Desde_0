e1f1048de814d6a77b082a8b877b220bfe79c5a6
JosePabonmejia <josefolofus@gmail.com>
2025-02-14 10:16:21 -0400

    2 Controladores

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function(){
+//     return "Esta es la pagina principal";
+// });
+
+// Route::get('/post',function(){
+//     return "ESTE ES EL POST";
+// });
+// Route::get('/post/block',function(){
+//     return "ESTE ES EL BLOCK";
+// });
+// Route::get('/post/{post}',function($post){
+//     return "ESTE ES EL {$post} ";
+// });
+// Route::get('/post/{post}/{category}',function($post,$category = "coca"){
+//     return "Se muestra el producto{$post}, de la categoria{$category}";
+// });
+
+
+
